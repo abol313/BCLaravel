631b03f3b8a833718a05a52f3c51b2e0265ba2cb
abol313 <FazlAbol18@gmail.com>
2022-06-16 17:39:18 +0430

    fix:RouteServiceProvider.php delete name to solve conflicts of laravel/ui

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,7 +34,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
-                ->name('route.')
+                //->name('route.')
                 ->group(base_path('routes/web.php'));
         });
     }
