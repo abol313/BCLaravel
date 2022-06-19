a07fb9bb05df12e275c03eeebdaefbe61da01718
abol313 <FazlAbol18@gmail.com>
2022-06-19 10:51:44 +0430

    fix:view.todo.make handled with session data (flashed)

--- resources/views/todo/make.blade.php
+++ resources/views/todo/make.blade.php
@@ -9,7 +9,9 @@
 @section('body')
     <h2 class="form-todo-make-title">Make a todo !</h2>
     @php
-        $success = $success ?? null;
+        $report = session('report');
+        $success = $report['success'] ?? null;
+        $message = $report['message'] ?? null;
     @endphp
     @if($success === true)
         <div class="todo-message todo-message-succeed">
@@ -20,8 +22,8 @@
             <h2>{{$message}}</h2>
         </div>
     @endif 
-    <form class="form-todo-make">
-
+    <form action="{{route('todo.makeAPI')}}" method="post" class="form-todo-make">
+        @csrf
         <label for="input_title" class="label-title" >Title</label>
         <input id="input_title" name="title" class="input-title" placeholder="Choose your title of todo" value="{{$attributes['title'] ?? null}}" required/>
 
@@ -33,11 +35,11 @@
 
         <label for="input_commander" class="label-commander">Commander</label>
         {{-- <input id="input_commander" name="commander" list="list_users" class="input-commander" placeholder="@@Set the commander (whos make this todo)" pattern="@.*" title="unique name must be started with @ character" value="{{$attributes['commander'] ?? null}}" required/> --}}
-        <input id="input_commander" name="commander" type="email" list="list_users" class="input-commander" placeholder="Set the commander (whos make this todo)" title="unique name must be started with @ character" value="{{$attributes['commander'] ?? null}}" required/>
+        <input id="input_commander" name="commander" type="email" list="list_users" class="input-commander" placeholder="Set the commander (whos make this todo)" title="unique name must be started with @ character" value="{{$attributes['commander'] ?? null}}" autocomplete="off" required/>
 
         <label for="input_soldier" class="label-soldier">Soldier</label>
         {{-- <input id="input_soldier" name="soldier" list="list_users" class="input-soldier" placeholder="@@Set your soldier (whos will get this todo)" pattern="@.*" title="unique name must be started with @ character" value="{{$attributes['soldier'] ?? null}}" required/> --}}
-        <input id="input_soldier" name="soldier" type="email" list="list_users" class="input-soldier" placeholder="Set your soldier (whos will get this todo)" title="unique name must be started with @ character" value="{{$attributes['soldier'] ?? null}}" required/>
+        <input id="input_soldier" name="soldier" type="email" list="list_users" class="input-soldier" placeholder="Set your soldier (whos will get this todo)" title="unique name must be started with @ character" value="{{$attributes['soldier'] ?? null}}" autocomplete="off" required/>
 
         <datalist id="list_users">
             @foreach(\App\Models\User::all() as $user)
@@ -45,6 +47,8 @@
                 <option value="{{$user->email}}"/>
             @endForeach
         </datalist>
+
+        <input type="hidden" name="redirectComeBack" value="true"/>
         <input type="reset" class="reset" value="Clean"/>
         <input type="submit" class="submit" value="Make !" />
     </form>
