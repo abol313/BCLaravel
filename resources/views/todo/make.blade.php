ba80641a2755febc8da655ff520baa8d2ae1b382
abol313 <FazlAbol18@gmail.com>
2022-06-19 12:11:05 +0430

    fix: handle the input values after get the response

--- resources/views/todo/make.blade.php
+++ resources/views/todo/make.blade.php
@@ -12,6 +12,7 @@
         $report = session('report');
         $success = $report['success'] ?? null;
         $message = $report['message'] ?? null;
+        $attributes = $report['attributes'] ?? null;
     @endphp
     @if($success === true)
         <div class="todo-message todo-message-succeed">
@@ -21,25 +22,31 @@
         <div class="todo-message todo-message-unsucceed">
             <h2>{{$message}}</h2>
         </div>
-    @endif 
+    @elseif($errors->any())
+        <div class="todo-message todo-message-unsucceed">
+            @foreach($errors->all() as $error)
+                <h2>{{$error}}</h2>
+            @endforeach
+        </div>
+    @endif
     <form action="{{route('todo.makeAPI')}}" method="post" class="form-todo-make">
         @csrf
         <label for="input_title" class="label-title" >Title</label>
-        <input id="input_title" name="title" class="input-title" placeholder="Choose your title of todo" value="{{$attributes['title'] ?? null}}" required/>
+        <input id="input_title" name="title" class="input-title" placeholder="Choose your title of todo" value="{{$attributes['title'] ?? old('title')}}" required/>
 
         <label for="textarea_description" class="label-description">Description</label>
-        <textarea id="textarea_description" name="description" class="textarea-description custom-scroll" rows="5" placeholder="Give descriptions..." required>{{$attributes['description'] ?? null}}</textarea>
+        <textarea id="textarea_description" name="description" class="textarea-description custom-scroll" rows="5" placeholder="Give descriptions..." required>{{$attributes['description'] ?? old('description')}}</textarea>
 
         <label for="input_due" class="label-due">Due (time)</label>
-        <input id="input_due" name="due" type="datetime-local" class="input-due" placeholder="Set the time should to be done" value="{{$attributes['due'] ?? null}}"/>
+        <input id="input_due" name="due" type="datetime-local" class="input-due" placeholder="Set the time should to be done" value="{{$attributes['due'] ?? old('due')}}"/>
 
         <label for="input_commander" class="label-commander">Commander</label>
         {{-- <input id="input_commander" name="commander" list="list_users" class="input-commander" placeholder="@@Set the commander (whos make this todo)" pattern="@.*" title="unique name must be started with @ character" value="{{$attributes['commander'] ?? null}}" required/> --}}
-        <input id="input_commander" name="commander" type="email" list="list_users" class="input-commander" placeholder="Set the commander (whos make this todo)" title="unique name must be started with @ character" value="{{$attributes['commander'] ?? null}}" autocomplete="off" required/>
+        <input id="input_commander" name="commander" type="email" list="list_users" class="input-commander" placeholder="Set the commander (whos make this todo)" title="unique name must be started with @ character" value="{{$attributes['commander'] ?? old('commander')}}" autocomplete="off" required/>
 
         <label for="input_soldier" class="label-soldier">Soldier</label>
         {{-- <input id="input_soldier" name="soldier" list="list_users" class="input-soldier" placeholder="@@Set your soldier (whos will get this todo)" pattern="@.*" title="unique name must be started with @ character" value="{{$attributes['soldier'] ?? null}}" required/> --}}
-        <input id="input_soldier" name="soldier" type="email" list="list_users" class="input-soldier" placeholder="Set your soldier (whos will get this todo)" title="unique name must be started with @ character" value="{{$attributes['soldier'] ?? null}}" autocomplete="off" required/>
+        <input id="input_soldier" name="soldier" type="email" list="list_users" class="input-soldier" placeholder="Set your soldier (whos will get this todo)" title="unique name must be started with @ character" value="{{$attributes['soldier'] ?? old('soldier')}}" autocomplete="off" required/>
 
         <datalist id="list_users">
             @foreach(\App\Models\User::all() as $user)
