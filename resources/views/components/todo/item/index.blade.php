4e1360d390894b1144325cfcfae5407f4f926a97
abol313 <FazlAbol18@gmail.com>
2022-06-19 23:51:08 +0430

    feat: component.todo add <x-todo.item.delete/>

--- resources/views/components/todo/item/index.blade.php
+++ resources/views/components/todo/item/index.blade.php
@@ -16,15 +16,24 @@
     }
 
     $deletePath = $id? route("todo.delete",['todo'=>$todo]):null;
+    $editPath = $id? route("todo.editView",[$todo]):null;
 @endphp
 <div  {!! $attributes->class(['todo-item']) !!}>
     <x-todo.item.container>
         <x-todo.item.title value="{!! $title !!}" />
+
         <x-todo.item.status value="{!! $status !!}" />
+
         <x-todo.item.description value="{!! $description !!}" />
+
         @unless(empty($due))
             <x-todo.item.due value="{!! $due !!}" />
         @endunless
+
+        @if($editPath)
+            <x-todo.item.edit :edit-path="$editPath"/>
+        @endif
+        
         @if($deletePath)
             <x-todo.item.delete :delete-path="$deletePath"/>
         @endif
