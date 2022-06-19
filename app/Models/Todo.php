654106a825faa578f783a4a396334e456ae390be
abol313 <FazlAbol18@gmail.com>
2022-06-19 10:58:03 +0430

    feat:Todo add fillables & fillIfPossible() method to more comfortable

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -3,8 +3,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Schema;
 
 class Todo extends Model {
     use HasFactory;
     protected $table = "todos";
+    protected $fillable = ['title','description','due','status'];
+    public function fillIfPossible(array $attributes){
+        $columnNames = Schema::getColumnListing($this->table);
+        $modelAttributes = Arr::only($attributes,$columnNames);
+        if(empty($modelAttributes))return $this;
+        $this->fill($modelAttributes);
+        return $this;
+    }
 }
