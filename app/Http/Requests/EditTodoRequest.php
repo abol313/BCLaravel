7c3672094346ba5692237a9773ed66e54ca52a28
abol313 <FazlAbol18@gmail.com>
2022-06-20 22:59:11 +0430

    Create:EditTodoRequest tp handle the request for editing todo

---
+++ app/Http/Requests/EditTodoRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditTodoRequest extends FormRequest
+{
+
+    private $editValidationRules = [
+        'title'=>'required|between:5,100',
+        'description'=>'nullable|max:65535',
+        // 'status'=>'nullable',
+        'due'=>'nullable|date|after:now',
+        'commander'=>'required|email|exists:users,email',
+        'soldier'=>'required|email|exists:users,email',
+        'todo'=>'required|exists:users_todos,todo'
+    ];
+
+
+    protected $stopOnFirstFailure = false;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+
+    public function prepareForValidation(){
+        $this->merge(['todo'=>$this->route('todo')->id]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return $this->editValidationRules;
+    }
+}
