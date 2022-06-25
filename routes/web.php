2331200e7be842308f57810582a0cf365d9683a6
abol313 <FazlAbol18@gmail.com>
2022-06-25 23:13:00 +0430

    fix:web.php clean the codes

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,9 @@
 
 use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-// use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 // use phpDocumentor\Reflection\Types\Boolean;
 
@@ -47,49 +45,6 @@ Route::controller(TodoController::class)
 
         });
 
-Route::get('/test',function(Request $request){
-    $validator = Validator::make(['name'=>'ali10'],[
-        'name'=>'required|integer'
-    ]);
-    $validator->stopOnFirstFailure();
-    dd($validator);
-});
 Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-/*practices
-*
-/
-
-Route::any('/any/{id1}',fn($id1)=>"any verb ;) $id1");
-
-Route::match(['get','post'],'/match',fn()=>"ijij")->name('route.match');
-
-Route::permanentRedirect('/a','/match',301);
-Route::view('/view','welcome');
-
-Route::pattern('name','\d+');
-Route::get('/optional/{name?}',function($a = 'No name',$name = ':/'){
-    return $a.$name;
-})->name('route.optional');
-
-Route::get('/b',fn(Request $request)=>response()->json([$request->route()->named('rote.a')]))->name('route.a');
-
-Route::prefix('/pre')->middleware('test')->controller(TodoController::class)
-    ->group(
-        function(){
-            Route::get('/list','listAll');
-        }
-    );
-
-Route::get('/usersTodos/{userTodo}/commanders/{user}',function(Models\UserTodo $userTodo, Models\User $user){
-    // foreach($todo as $t)
-    //     print_r($t->getAttributes());
-    return response()->json(['todo'=>($userTodo->getAttributes()),'user'=>($user->getAttributes())]);
-})->where('todo','.*')->missing(fn()=>response()->json(["No matched!"]));
-
-
-Route::get('/category/{category}',function(Category  $category){
-    return $category->value;
-});
-//*/
