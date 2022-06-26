9c5f4a0cb1aa8b8868bb6153e51e9e59d0e62bde
abol313 <FazlAbol18@gmail.com>
2022-06-26 22:06:15 +0430

    feat: add lang files to handle the

---
+++ lang/en/todo.php
@@ -0,0 +1,76 @@
+<?php
+return [
+    'title'=>'Todo Collection',
+    'welcome'=>'Welcome to Todo-Collection',
+    'list'=>'List',
+    'make'=>'Make',
+
+    'form'=>[
+        'make'=>[
+            'form_title'=>'Make a todo !',
+
+            'title'=>'title',
+            'title_placeholder'=>'Choose your title of todo',
+            
+            'status'=>'status',
+            'status_placeholder'=>'Set your status of todo',
+            
+            'description'=>'description',
+            'description_placeholder'=>'Give descripitons...',
+        
+            'due'=>'due',
+            'due_placeholder'=>'Set a time to do',
+        
+            'commander'=>'commander',
+            'commander_placeholder'=>'Set the commander',
+        
+            'soldier'=>'soldier',
+            'soldier_placeholder'=>'Set the soldier',
+        
+            'form_reset'=>'Clean',
+            'form_submit'=>'Make !',
+        
+        ],
+        'edit'=>[
+            'form_title'=>'Edit the todo !',
+
+            'title'=>'title',
+            'title_placeholder'=>'Choose your title of todo',
+            
+            'status'=>'status',
+            'status_placeholder'=>'Set your status of todo',
+            
+            'description'=>'description',
+            'description_placeholder'=>'Give descripitons...',
+        
+            'due'=>'due',
+            'due_placeholder'=>'Set a time to do',
+        
+            'commander'=>'commander',
+            'commander_placeholder'=>'Set the commander',
+        
+            'soldier'=>'soldier',
+            'soldier_placeholder'=>'Set the soldier',
+        
+            'form_reset'=>'Clean',
+            'form_submit'=>'Edit !',
+        
+        ]
+    ],
+
+    'sure'=>'Are you sure?',
+
+    'todo'=>[
+        "status"=>[
+            "waiting"=>"waiting",
+            "accepted"=>"accepted",
+            "declined"=>"declined",
+            "done"=>"done",
+        ],
+        'edit'=>'Edit',
+        'delete'=>'Delete',
+    ],
+
+    'no_todo'=>'No todo !',
+    'made'=>'Made with interest ;)',
+];
---
+++ lang/fa/todo.php
@@ -0,0 +1,75 @@
+<?php
+return [
+    'title'=>'لیست ماموریت',
+    'welcome'=>'به لیست-ماموریت خوش آمدید',
+    'list'=>'لیست',
+    'make'=>'بساز',
+
+    'form'=>[
+        'make'=>[
+            'form_title'=>'یک ماموریت بساز !',
+
+            'title'=>'عنوان',
+            'title_placeholder'=>'عنوان ماموریتت رو انتخاب کن',
+            
+            'status'=>'حالت',
+            'status_placeholder'=>'حالت ماموریتت رو انتخاب کن',
+            
+            'description'=>'توضیح',
+            'description_placeholder'=>'توضیحاتی بده...',
+        
+            'due'=>'موعد',
+            'due_placeholder'=>'یک زمانی را برای انجام ماموریت تعیین کن',
+        
+            'commander'=>'فرمانده',
+            'commander_placeholder'=>'فرمانده را تعیین کن',
+        
+            'soldier'=>'سرباز',
+            'soldier_placeholder'=>'سرباز را تعیین کن',
+        
+            'form_reset'=>'پاک کن',
+            'form_submit'=>'بساز !',
+        
+        ],
+        'edit'=>[
+            'form_title'=>'ماموریت رو ویرایش کن !',
+
+            'title'=>'عنوان',
+            'title_placeholder'=>'عنوان ماموریتت رو انتخاب کن',
+            
+            'status'=>'حالت',
+            'status_placeholder'=>'حالت ماموریتت رو انتخاب کن',
+            
+            'description'=>'توضیح',
+            'description_placeholder'=>'توضیحاتی بده...',
+        
+            'due'=>'موعد',
+            'due_placeholder'=>'یک زمانی را برای انجام ماموریت تعیین کن',
+        
+            'commander'=>'فرمانده',
+            'commander_placeholder'=>'فرمانده را تعیین کن',
+        
+            'soldier'=>'سرباز',
+            'soldier_placeholder'=>'سرباز را تعیین کن',
+        
+            'form_reset'=>'پاک کن',
+            'form_submit'=>'تغییر بده !',        
+        ]
+    ],
+
+    'sure'=>'مطمئنی?',
+
+    'todo'=>[
+        "status"=>[
+            "waiting"=>"منتظر",
+            "accepted"=>"مقبول",
+            "declined"=>"مردود",
+            "done"=>"حله",
+        ],
+        'edit'=>'ویرایش',
+        'delete'=>'حذف',
+    ],
+
+    'no_todo'=>'بدون ماموریت !',
+    'made'=>'ساخته شده با علاقه !',
+];
