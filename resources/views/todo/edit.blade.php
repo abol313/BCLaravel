fa166255d351b80295cc5588708d866005642f90
abol313 <FazlAbol18@gmail.com>
2022-06-26 10:33:44 +0430

    fix:handle errors messages in edit form such as create form

--- resources/views/todo/edit.blade.php
+++ resources/views/todo/edit.blade.php
@@ -23,32 +23,51 @@
         <div class="todo-message todo-message-unsucceed">
             <h2>{{$message}}</h2>
         </div>
-    @elseif($errors->any())
-        <div class="todo-message todo-message-unsucceed">
-            @foreach($errors->all() as $error)
-                <h2>{{$error}}</h2>
-            @endforeach
-        </div>
     @endif
     
     <form action="{{route('todo.update',[$todo])}}" method="post" class="form-todo-make">
         @csrf
         <label for="input_title" class="label-title" >Title</label>
-        <input id="input_title" name="title" class="input-title" placeholder="Choose your title of todo" value="{{$attributes['title'] ?? old('title') ?? $todo->getAttribute('title') }}" required/>
+        <input id="input_title" name="title" class="input-title @error('title') input-validation-error  @enderror" placeholder="Choose your title of todo" value="{{$attributes['title'] ?? old('title') ?? $todo->getAttribute('title') }}" required/>
+        @error('title')
+            <div class="todo-message message-validation-error">
+                <h2>{{$message}}</h2>
+            </div>
+        @enderror
 
         <label for="textarea_description" class="label-description">Description</label>
-        <textarea id="textarea_description" name="description" class="textarea-description custom-scroll" rows="5" placeholder="Give descriptions..." required>{{$attributes['description'] ?? old('description') ?? $todo->getAttribute('description')}}</textarea>
+        <textarea id="textarea_description" name="description" class="textarea-description custom-scroll @error('description') input-validation-error  @enderror" rows="5" placeholder="Give descriptions..." required>{{$attributes['description'] ?? old('description') ?? $todo->getAttribute('description')}}</textarea>
+        @error('description')
+            <div class="todo-message message-validation-error">
+                <h2>{{$message}}</h2>
+            </div>
+        @enderror
 
         <label for="input_due" class="label-due">Due (time)</label>
-        <input id="input_due" name="due" type="datetime-local" class="input-due" placeholder="Set the time should to be done" value="{{str_replace(' ','T',$attributes['due'] ?? old('due') ?? $todo->getAttribute('due'))}}"/>
+        <input id="input_due" name="due" type="datetime-local" class="input-due @error('due') input-validation-error  @enderror" placeholder="Set the time should to be done" value="{{str_replace(' ','T',$attributes['due'] ?? old('due') ?? $todo->getAttribute('due'))}}"/>
+        @error('due')
+            <div class="todo-message message-validation-error">
+                <h2>{{$message}}</h2>
+            </div>
+        @enderror
 
         <label for="input_commander" class="label-commander">Commander</label>
         {{-- <input id="input_commander" name="commander" list="list_users" class="input-commander" placeholder="@@Set the commander (whos make this todo)" pattern="@.*" title="unique name must be started with @ character" value="{{$attributes['commander'] ?? null}}" required/> --}}
-        <input id="input_commander" name="commander" type="email" list="list_users" class="input-commander" placeholder="Set the commander (whos make this todo)" title="unique name must be started with @ character" value="{{$attributes['commander'] ?? old('commander') ?? $commander ?? null}}" autocomplete="off" required/>
+        <input id="input_commander" name="commander" type="email" list="list_users" class="input-commander @error('commander') input-validation-error  @enderror" placeholder="Set the commander (whos make this todo)" title="unique name must be started with @ character" value="{{$attributes['commander'] ?? old('commander') ?? $commander ?? null}}" autocomplete="off" required/>
+        @error('commander')
+            <div class="todo-message message-validation-error">
+                <h2>{{$message}}</h2>
+            </div>
+        @enderror
 
         <label for="input_soldier" class="label-soldier">Soldier</label>
         {{-- <input id="input_soldier" name="soldier" list="list_users" class="input-soldier" placeholder="@@Set your soldier (whos will get this todo)" pattern="@.*" title="unique name must be started with @ character" value="{{$attributes['soldier'] ?? null}}" required/> --}}
-        <input id="input_soldier" name="soldier" type="email" list="list_users" class="input-soldier" placeholder="Set your soldier (whos will get this todo)" title="unique name must be started with @ character" value="{{$attributes['soldier'] ?? old('soldier') ?? $soldier ?? null}}" autocomplete="off" required/>
+        <input id="input_soldier" name="soldier" type="email" list="list_users" class="input-soldier @error('soldier') input-validation-error  @enderror" placeholder="Set your soldier (whos will get this todo)" title="unique name must be started with @ character" value="{{$attributes['soldier'] ?? old('soldier') ?? $soldier ?? null}}" autocomplete="off" required/>
+        @error('soldier')
+            <div class="todo-message message-validation-error">
+                <h2>{{$message}}</h2>
+            </div>
+        @enderror
 
         <datalist id="list_users">
             @foreach(\App\Models\User::all() as $user)
