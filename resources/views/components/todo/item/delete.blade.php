14e2a4afb08b98804d36e8485e2dfc0db19fe93c
abol313 <FazlAbol18@gmail.com>
2022-06-26 23:11:09 +0430

    fix:view.component.todo.delete debug the sur ask

--- resources/views/components/todo/item/delete.blade.php
+++ resources/views/components/todo/item/delete.blade.php
@@ -1,6 +1,6 @@
 @aware(['deletePath'])
 <div class="delete">
-    <a href="{{$deletePath}}" onclick="return confirm({{__("todo.sure")}})">
+    <a href="{{$deletePath}}" onclick='return confirm("{{__("todo.sure")}}")'>
         <h2>
             @lang("todo.todo.delete")
         </h2>
