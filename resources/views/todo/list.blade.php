0d1b4851b15122349a42937b4f65d67b6ad332b6
abol313 <FazlAbol18@gmail.com>
2022-07-01 19:16:48 +0430

    fix:view.todo.list add a fake todo element to use in todoManager.js

--- resources/views/todo/list.blade.php
+++ resources/views/todo/list.blade.php
@@ -1,16 +1,23 @@
 @extends('layouts.app-todo')
 
 @php
-    use App\Models\Todo; 
+use App\Models\Todo;
 @endphp
-{{-- @push('styles')
-    <link rel="stylesheet" href="{{mix('css/main.css')}}"/>
-@endPush --}}
 
 @section('body')
+    @php
+    $fakeTodo = new Todo();
+    $fakeTodo->id = -1;
+    $fakeTodo->title = 'title';
+    $fakeTodo->status = 'waiting';
+    $fakeTodo->description = 'description';
+    $fakeTodo->due = '2020-12-20 12:23:00';
+    @endphp
+    <x-todo.item :todo="$fakeTodo" style="display:none" />
+
     @forelse($todos as $todo)
-        <x-todo.item :todo="$todo"/>
+        <x-todo.item :todo="$todo" />
     @empty
-        <x-todo.empty/>
+        <x-todo.empty />
     @endforelse
 @endsection
