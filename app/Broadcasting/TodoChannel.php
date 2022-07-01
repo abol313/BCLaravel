df1fcc24f31d3d02da29a47bbebadc5c9a8b7126
abol313 <FazlAbol18@gmail.com>
2022-07-01 19:48:15 +0430

    feat:TodoChannel add to auth the websockets requests

---
+++ app/Broadcasting/TodoChannel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Broadcasting;
+
+use App\Models\Todo;
+use App\Models\User;
+
+class TodoChannel
+{
+    /**
+     * Create a new channel instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Authenticate the user's access to the channel.
+     *
+     * @param  \App\Models\User  $user
+     * @return array|bool
+     */
+    public function join(User $user,$todo)
+    {
+        //
+        if($todo==="x")return true;
+        $todo = Todo::find($todo);
+        return $todo->hasRelationToUsers()?->commander === $user->id || $todo->hasRelationToUsers()?->soldier === $user->id;
+    }
+}
